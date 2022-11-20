756b7ce5207848d16060dc2156e9a1ffebf899af
Hulend <59649037+Hulend@users.noreply.github.com>
2022-11-20 00:12:54 -0800

    Lab 8 All-in-One

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Lab 8 - Starter
+Michael Tedjo <br>
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+   1. Within a Github action that runs whenever code is pushed 
+   2. Manually run them locally before pushing code
+   3. Run them all after all development is completed
+
+I would choose within a Github action since making sure that the code meets all criteria when being pushed to a branch allows us to see if it has diminished or broken in functionality.
+
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+No, I would use a unit test for a function rather than an end to end test since its quicker and tests the function directly.
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No I would not use a unit test to test the "message" feature: unit testing would be for the write function and the send function. When those two are functional, then the interaction between the two would work as well. (Remember Single Responbility Principle: Each function to achieve one thing.)
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes a unit test is applicable here. The feature is comprised of only one function; to say if a message is too long or not.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,87 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// isPhoneNumber(phoneNumber)
+test('valid phoneNumber 1', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('valid phoneNumber 2', () => {
+    expect(functions.isPhoneNumber('(923)324-7850')).toBe(true);
+});
+
+test('invalid phoneNumber 1', () => {
+    expect(functions.isPhoneNumber('12-45-78')).toBe(false);
+});
+
+test('invalid phoneNumber 2', () => {
+    expect(functions.isPhoneNumber('((111))456))(7890')).toBe(false);
+});
+
+// isEmail(email) 
+test('valid email 1', () => {
+    expect(functions.isEmail('a@y.com')).toBe(true);
+});
+
+test('valid email 2', () => {
+    expect(functions.isEmail('b@x.edu')).toBe(true);
+});
+
+test('invalid email 1', () => {
+    expect(functions.isEmail('boy@yamato.long')).toBe(false);
+});
+
+test('invalid email 2', () => {
+    expect(functions.isEmail('111-222-3333')).toBe(false);
+});
+
+// isStrongPassword(password)
+test('valid password 1', () => {
+    expect(functions.isStrongPassword('a23def7890')).toBe(true);
+});
+
+test('valid password 2', () => {
+    expect(functions.isStrongPassword('Password')).toBe(true);
+});
+
+test('invalid password 1', () => {
+    expect(functions.isStrongPassword('1girl')).toBe(false);
+});
+
+test('invalid password 2', () => {
+    expect(functions.isStrongPassword('3*sJi@kLsd')).toBe(false);
+});
+
+// isDate(date)
+test('valid date 1', () => {
+    expect(functions.isDate('12/1/1234')).toBe(true);
+});
+
+test('valid date 2', () => {
+    expect(functions.isDate('9/98/9876')).toBe(true);
+});
+
+test('invalid date 1', () => {
+    expect(functions.isDate('10/4/12')).toBe(false);
+});
+
+test('invalid date 2', () => {
+    expect(functions.isDate('11-19-2022')).toBe(false);
+});
+
+// isHexColor(color)
+test('valid hex color 1', () => {
+    expect(functions.isHexColor('#a2c')).toBe(true);
+});
+
+test('valid hex color 2', () => {
+    expect(functions.isHexColor('1f2d0e')).toBe(true);
+});
+
+test('invalid hex color 1', () => {
+    expect(functions.isHexColor('#12g')).toBe(false);
+});
+
+test('invalid hex color 2', () => {
+    expect(functions.isHexColor('8f8ea')).toBe(false);
+});
